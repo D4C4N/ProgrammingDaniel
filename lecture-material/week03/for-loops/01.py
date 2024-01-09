6b5122d97ddaaa4a7300272f2cdd1d5463d0fabf
D4C4N <dacan1508@gmail.com>
2024-01-09 20:40:07 +0100

    Restructred and cleaned up files.

---
+++ lecture-material/week03/for-loops/01.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    for j in range(i):
+        print("[]", end="")
+    print(" ")
+
+
+for l in range(5, -1, -1):
+    for k in range(l):
+        print("[]", end="")
+    print(" ")
